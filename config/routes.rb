97ef4f9b1685d27b53cd95f7611679fbccd20299
わたり。 <yue.hinako.0175@gmail.com>
2019-07-26 17:07:11 +0900

    front for signup and users manegiment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
+root 'messages#index'
   devise_for :users
     resources :messages, only: [:index, :create]
-
+    resources :users, only: [:index, :edit, :update]
 
 end
